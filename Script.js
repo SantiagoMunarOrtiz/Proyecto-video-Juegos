973b3a27132941467af491ac2702e13504de4fda
Juliana Andrea Caicedo Avila <bathory2909@gmail.com>
2024-04-06 15:49:28 -0500

    EL MUÑECO YA SALTA Y EL FONDO SE MUEVE

--- Script.js
+++ Script.js
@@ -1,4 +1,11 @@
-const jugador= document.getElementById("jugador");
-document.addEventListener("click",function(){
-    jugador.classList.add("jugadorSaltar");
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const jugador = document.getElementById("jugador");
+    const jugadorSaltar = document.getElementById("jugadorSaltar");
+
+    jugadorSaltar.addEventListener("click", function(){
+        jugador.classList.add("jugadorSaltar");
+        setTimeout(function(){
+            jugador.classList.remove("jugadorSaltar");
+        }, 1000); // Duración de la animación en milisegundos
+    });
+});
