abafee7b0b8ddebc7c26bac51c7f209c12dbe6e5
Juliana Andrea Caicedo Avila <bathory2909@gmail.com>
2024-04-06 23:12:14 -0500

    Tiene botones, falta la perder

--- Script.js
+++ Script.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const jugador = document.getElementById("jugador");
-    const jugadorSaltar = document.getElementById("jugadorSaltar");
+const Hogo = document.getElementById("Hogo");
+const jugador = document.getElementById("jugador");
+const jugadorSaltar = document.getElementById("jugadorSaltar");
+const Murcielago = document.getElementById("Murcielago");
+const fondo = document.getElementById("fondo");
 
+let contador = 0;
+let intervaloContador;
+
+document.addEventListener("DOMContentLoaded", function() {
     jugadorSaltar.addEventListener("click", function(){
         jugador.classList.add("jugadorSaltar");
         setTimeout(function(){
@@ -9,10 +15,58 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 1000); 
     });
 });
+    BotonPausa.addEventListener("click", ()=>{
+        Pausa(); 
+    })
+    function Pausa(){
+        Hogo.style.animationPlayState="paused";
+        Murcielago.style.animationPlayState="paused";
+        jugador.style.animationPlayState="paused";
+        fondo.style.animationPlayState="paused";
+        clearInterval(intervaloContador);
+    }
+    BotonPlay.addEventListener("click", ()=>{
+        Play(); 
+    })
+    
+    function Play(){
+        Hogo.style.animationPlayState="running";
+        Murcielago.style.animationPlayState="running";
+        jugador.style.animationPlayState="running";
+        fondo.style.animationPlayState="running";
 
+        intervaloContador = setInterval(()=>{
+            contador++;
+            document.getElementById("contador").innerText=contador;
+        }, 100);
+    }
+ intervaloContador = setInterval(()=>{
+    contador++;
+    document.getElementById("contador").innerText=contador;
+}, 100);
+
+BotonPlay.addEventListener("click", ()=>{
+    Play(); 
+})
+BotonReanudar.addEventListener("click", ()=>{
+    BotonReanudar();
+})
+
+function BotonReanudar(){
+    alert("Se va a reiniciar el juego .-.")
+}
 
 
 function Perder() {
-    const confirmacion = window.confirm('Perdiste :(');
-  
-  }
+    var jugadorLeft = parseInt(jugador.style.left);
+    var jugadorTop = parseInt(jugador.style.top);
+    var hogoLeft = parseInt(Hogo.style.left);
+    var hogoTop = parseInt(Hogo.style.top);
+
+    if (jugadorLeft === hogoLeft && jugadorTop === hogoTop) {
+        alert("¡Perdiste!");
+        console.log("¡Perdiste!");
+
+    }
+}
+ 
